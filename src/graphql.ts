99f4c5612e86c7f759a7965130a19589095bbc7c
baronchibuikem <baronchibuike@gmail.com>
2020-12-14 12:39:47 +0100

    Added listing and deleteListing endpoint on graphl

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,21 +1,72 @@
-import { GraphQLSchema, GraphQLString, GraphQLObjectType } from 'graphql'
+import { GraphQLSchema, GraphQLString, GraphQLObjectType,
+     GraphQLID, GraphQLInt, GraphQLFloat, GraphQLNonNull, GraphQLList} from 'graphql'
+import { listings } from "./listings";
+
+
+const Listing = new GraphQLObjectType({
+    name: "Listing",
+    fields: {
+        id: {
+            type: GraphQLNonNull(GraphQLID)
+        },
+        title: {
+            type: GraphQLNonNull(GraphQLString),
+        },
+        image: {
+            type: GraphQLNonNull(GraphQLString),
+        },
+        address: {
+            type: GraphQLNonNull(GraphQLString),
+        },
+        price: {
+            type: GraphQLNonNull(GraphQLInt)
+        },
+        numOfGuests: {
+            type: GraphQLNonNull(GraphQLInt)
+        },
+        numOfBeds: {
+            type: GraphQLNonNull(GraphQLInt)
+        },
+        numOfBaths: {
+            type: GraphQLNonNull(GraphQLInt)
+        },
+        rating: {
+            type: GraphQLNonNull(GraphQLFloat)
+        }
+    }
+})
 
 const query = new GraphQLObjectType({
     name: "Query",
     fields:{
-        hello: {
-            type: GraphQLString,
-            resolve: () => "Hello from the query"
-        }
+       listings: {
+           type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+           resolve: () => {
+               return listings
+           }
+       }
     }
 })
 
 const mutation = new GraphQLObjectType({
     name: "Mutation",
     fields:{
-        hello: {
-            type: GraphQLString,
-            resolve: () => "hello from the mutation"
+        deleteListing: {
+            type: GraphQLNonNull(Listing),
+            // this will be used to accept an argument
+            args: {
+                id: {
+                    type: GraphQLNonNull(GraphQLID)
+                }
+            },
+                resolve: (_root, { id }) => {
+                    for(let i=0; i < listings.length; i++){
+                        if(listings[i].id === id){
+                            return listings.splice(i, 1)[0]
+                        }
+                    }
+                    throw new Error("failed to delete listing")
+                }
         }
     }
 })
